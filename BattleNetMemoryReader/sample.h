6f70940fefc2f673bcfdc4abc310b529f201a47f
shyft <93425247+imshyft@users.noreply.github.com>
2025-12-31 23:48:38 +1100

    Removed unnecessary directories

--- BattleNetMemoryReader/sample.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector>
-#include <list>
-
-
-extern "C" _declspec(dllexport) const char** GetFriendBattleTags(HANDLE Handle, int* outSize);
-extern "C" _declspec(dllexport) const char** GetUserBattleTag(HANDLE Handle, int* outSize);
